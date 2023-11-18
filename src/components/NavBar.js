348b1ed47023388586dcfd10aba47806d9c5b1a8
Azani David Pinkney <info@azani.org>
2023-11-18 11:43:43 -0800

    list and keys lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,20 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksDisplay = links.map((link) => {
+    const href = `#${link}`;
+    return (
+      <a key={link} href={href}>{link}</a>
+    )
+  })
+
+
+  return (
+    <nav>
+      {linksDisplay}
+    </nav>
+  )
+
 }
 
 export default NavBar;
